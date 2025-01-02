a1c268bf7fb7e9541a323cd37910a6e6edee7fc2
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2025-01-02 21:19:12 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,13 @@
+var map = L.map('map').setView([48.8566, 2.3522], 5);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+fetch('/api/targets')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(target => {
+            L.marker([target.lat, target.lon])
+                .addTo(map)
+                .bindPopup(`<b>${target.name}</b><br>${target.industry}`);
+        });
+    });
