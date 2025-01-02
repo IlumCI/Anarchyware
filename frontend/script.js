1b1e9492640e2ed5c219ef9b494d29a273b4d5a6
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2025-01-02 21:22:29 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,12 +2,24 @@ var map = L.map('map').setView([48.8566, 2.3522], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
+function getMarkerColor(score) {
+    if (score < 40) return 'green';
+    if (score < 70) return 'orange';
+    return 'red';
+}
+
 fetch('/api/targets')
     .then(response => response.json())
     .then(data => {
         data.forEach(target => {
-            L.marker([target.lat, target.lon])
-                .addTo(map)
-                .bindPopup(`<b>${target.name}</b><br>${target.industry}`);
+            let marker = L.circleMarker([target.lat, target.lon], {
+                radius: 8,
+                color: getMarkerColor(target.score)
+            }).addTo(map)
+            .bindPopup(`
+                <b>${target.name}</b><br>
+                Industry: ${target.industry}<br>
+                Score: ${target.score}/100
+            `);
         });
     });
